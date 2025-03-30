0821c68c67d6e501af3e346c7cfa1dafbf461ddb
ASPactores <anakin.pactores@gmail.com>
2025-03-31 02:43:07 +0800

    feat: Initialize backend with FastAPI and basic structure

---
+++ backend/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from mangum import Mangum
+
+app = FastAPI(
+    root_path="/api/v1",
+)
+origins = ['*']
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def hello_world():
+    return {"message": "Hello World"}
+
+handler = Mangum(
+    app,
+    api_gateway_base_path="/api/v1",
+)
