1dbaf31063836fb0c113f11236ef69113c717c4e
Francis Reid Arranguez <fnarranguez@up.edu.ph>
2025-04-06 22:52:33 +0800

    chore: initialized branch for admin login and qr code scanner

--- frontend/src/pages/Login.tsx
+++
@@ -1,9 +0,0 @@
-export default function LoginPage() {
-  return (
-    <div>
-      <h1>Login Page</h1>
-      <p>Please enter your credentials to log in.</p>
-      {/* Add your login form here */}
-    </div>
-  );
-}
